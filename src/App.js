70f5ecc3920aec8bcf27b9697335b5ea47801831
Arpan Neupane <arpanneupane19@gmail.com>
2021-04-24 13:44:01 -0500

    added React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Home from './components/Home';
+import Recents from './components/Recents';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
     return (
-        <div>
-            <Home/>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path='/'>
+                    <Home />
+                </Route>
+            </Switch>
+            <Switch>
+                <Route exact path='/recents'>
+                    <Recents />
+                </Route>
+            </Switch>
+        </Router>
     )
 
 }
