8d4cc177715423630f9a8d5b6ea4edee9bd29e94
Arpan Neupane <arpanneupane19@gmail.com>
2021-11-12 23:13:02 -0600

    added https redirect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import Weather from './components/Weather';
-import Recents from './components/Recents';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import dotenv from 'dotenv'
+import React from "react";
+import Weather from "./components/Weather";
+import Recents from "./components/Recents";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import dotenv from "dotenv";
+import HttpsRedirect from "react-https-redirect";
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path={['/', '/home']}>
-                    <Weather />
-                </Route>
-            </Switch>
-            <Switch>
-                <Route exact path='/recents'>
-                    <Recents />
-                </Route>
-            </Switch>
-        </Router>
-    )
-
+  return (
+    <HttpsRedirect>
+      <Router>
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+            <Weather />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route exact path="/recents">
+            <Recents />
+          </Route>
+        </Switch>
+      </Router>
+    </HttpsRedirect>
+  );
 }
 
 export default App;
